3714298c55bf9955608fa58bc43d1e85bc14cee1
ARTI1208 <leonardo906@mail.ru>
2020-09-18 13:16:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/smeshariki/emotions/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.smeshariki.emotions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.smeshariki.emotions.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
